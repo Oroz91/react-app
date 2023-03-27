b107cd33226aa4c446f42ed8778f83ee500f3b23
grafika10 <grafika10@ucione.local>
2023-03-27 21:59:10 +0200

    projekt

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/header";
 import Navigation from "./components/navigation";
 import "./styles/styles.scss";
+import hamburger from "./assets/icon-hamburger.svg";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Navigation />
+      <div>Hello World!</div>
     </div>
   );
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
 import logo from "./../assets/logo.png";
+import hamburger from "./../assets/icon-hamburger.svg";
 
 const Header = () => {
   return (
     <header className="header">
+      <div className="header__inner"></div>
       <div>
         <img className="header__logo" src={logo} alt="Logo" />
       </div>
@@ -16,6 +18,9 @@ const Header = () => {
           </a>
         </nav>
         <div>IS</div>
+      </div>
+      <div className="header__hamburger">
+        <img src={hamburger} alt="hamburger" />
       </div>
     </header>
   );
